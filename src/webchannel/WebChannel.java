8f794bf2daac8274e0a1fad20903d706b180bfe4
John Muellerleile <jmuellerleile@gmail.com>
2013-03-16 23:10:18 -0700

    json, websocket server test snippets

--- src/webchannel/WebChannel.java
+++ src/webchannel/WebChannel.java
@@ -1,11 +1,90 @@
 package webchannel;
 
-import java.util.*;
+import org.json.simple.JSONObject;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
 
-public class WebChannel {
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.framing.FrameBuilder;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
 
+public class WebChannel extends WebSocketServer {
+    
 	public static void main(String args[]) throws Exception {
 		System.out.println("webchannel");
+		
+		// json object
+		
+        JSONObject obj=new JSONObject();
+        obj.put("name","foo");
+        obj.put("num",new Integer(100));
+        obj.put("balance",new Double(1000.21));
+        obj.put("is_vip",new Boolean(true));
+        obj.put("nickname",null);
+        System.out.println(obj);
+        
+        // websocket server
+        
+        WebSocketImpl.DEBUG = false;
+        int port;
+        
+        try {
+            port = new Integer( args[ 0 ] );
+        } catch ( Exception e ) {
+            System.out.println( "No port specified. Defaulting to 9003" );
+            port = 9003;
+        }
+        
+        System.out.println("starting websocket server");
+        new WebChannel(port, new Draft_17() ).start();
+	}
+	
+	// websocket impl
+	
+    private int port;
+    public WebChannel(int port, Draft draft) throws UnknownHostException {
+        super(new InetSocketAddress(port), Collections.singletonList(draft));
+    }
+	
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+	   System.out.println("websocket: opened connection");
 	}
 
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+	   System.out.println("websocket: closed connection");
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+	   System.out.println("websocket: error: ");
+	   ex.printStackTrace();
+	}
+
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+	   System.out.println("onMessage: " + conn + ": " + message);
+	   conn.send(message);
+	}
+
+	@Override
+	public void onMessage(WebSocket conn, ByteBuffer blob) {
+	   System.out.println("onMessage: " + conn + ": " + blob);
+	   conn.send(blob);
+	}
+
+	@Override
+	public void onWebsocketMessageFragment(WebSocket conn, Framedata frame) {
+	   FrameBuilder builder = (FrameBuilder) frame;
+	   builder.setTransferemasked(false);
+	   conn.sendFrame(frame);
+	}
 }
